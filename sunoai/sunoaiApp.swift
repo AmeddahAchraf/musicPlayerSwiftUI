40f73ad6bdc05dd9492f803fc9ccb50ebc4c1fd3
AmeddahAchraf <achraf.ameddah@egym.com>
2024-10-13 18:51:38 +0200

    Initial Commit

---
+++ sunoai/sunoaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunoaiApp.swift
+//  sunoai
+//
+//  Created by Achraf Ameddah on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct sunoaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
