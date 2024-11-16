92058a59f9991e1eef5689de621ac21bd1d567e0
AmeddahAchraf <achraf.ameddah@egym.com>
2024-11-16 18:09:40 +0100

    [Add] network layer

---
+++ sunoai/Network/NetworkService.swift
@@ -0,0 +1,50 @@
+import Combine
+import Foundation
+
+final class NetworkService: NetworkServiceProtocol {
+    static let shared = NetworkService()
+
+    private init() {}
+
+    private var defaultHeaders: [String: String] {
+        [
+            "Authorization": "Bearer",
+            "Content-Type": "text/plain; charset=utf-8"
+        ]
+    }
+
+    func request<T: Decodable>(
+        _ request: RequestProtocol
+    ) -> AnyPublisher<T, Error> {
+        guard var url = URL(string: NetworkConfiguration.current.baseURL) else {
+            fatalError("Error creating base URL")
+        }
+
+        var resolvedPath = request.path
+        if let pathParameters = request.pathParameters {
+            for (key, value) in pathParameters {
+                resolvedPath = resolvedPath.replacingOccurrences(of: "{\(key)}", with: value)
+            }
+        }
+
+        url.appendPathComponent(resolvedPath)
+
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = request.method.rawValue
+
+        defaultHeaders.forEach { urlRequest.setValue($1, forHTTPHeaderField: $0) }
+
+        if let parameters = request.bodyParameters {
+            do {
+                urlRequest.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            } catch {
+                return Fail(error: error).eraseToAnyPublisher()
+            }
+        }
+
+        return URLSession.shared.dataTaskPublisher(for: urlRequest)
+            .map { $0.data }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
